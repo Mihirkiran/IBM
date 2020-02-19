b5d94964a62e3d383fe6e34a14cdb1c052c21137
Mihirkiran <44088004+Mihirkiran@users.noreply.github.com>
2020-02-19 15:12:13 +0530

    Add files via upload

--- 18-02-2020/Login/src/com/ibm/training/login.java
+++ 18-02-2020/Login/src/com/ibm/training/login.java
@@ -1,35 +1,72 @@
 package com.ibm.training;
 
-import java.io.IOException;
+import java.io.IOException;  
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-
-@WebServlet("/test")
-public class login extends HttpServlet {
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		if(request.getParameter("userName").equals("mihir") && request.getParameter("password").equals("mihir")) {
-			HttpSession session = request.getSession();
-//			HttpSession session1 = request.getSession();
-			session.setAttribute("userName", request.getParameter("userName"));
-			session.setAttribute("password", request.getParameter("password"));
-			response.sendRedirect("logout.html");
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+import javax.servlet.http.HttpSession;  
+@WebServlet("/loginpage")
+public class login extends HttpServlet { 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)  
+                    throws ServletException, IOException { 
+//    	HttpSession session;
+    	String p = "";
+    	Connection dbCon = null;
+    	ResultSet rs;
+        PrintWriter out=response.getWriter(); 
+    	try {
+    		Class.forName("com.mysql.cj.jdbc.Driver");
+			dbCon = DriverManager.getConnection("jdbc:mysql://localhost:3306/login?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+    	catch (ClassNotFoundException e) {
+    		System.out.println("jksdnbkdjs");
 		}
-		
-		PrintWriter out = response.getWriter();
-		
-//		out.println("Welcome " + request.getParameter("userName"));
-//		out.println("UserName in session : " + request.getSession().getAttribute("userName"));
-			
-	}
-
-}
+    	        
+    	
+	    	response.setContentType("text/html");  
+	 
+	        request.getRequestDispatcher("link.html").include(request, response);  
+	          
+	        String name=request.getParameter("name");  
+	        String password=request.getParameter("password");  
+	          
+	        String selectQry = "select * from userdetails where userID = ?";
+	        
+			try {
+				PreparedStatement stmt = dbCon.prepareStatement(selectQry);
+				stmt.setString(1, name);
+				rs = stmt.executeQuery();
+	//			p = rs.getString("password");
+				if(rs.next()) {
+		        	p = rs.getString("password"); 
+		        }
+			} catch (SQLException e) {
+				System.out.println("ABC");
+			} catch (NullPointerException e) {
+				System.out.println("DEF");
+			}
+	
+	        if(password.equals(p)){  
+	        	out.print("Welcome, "+name);  
+	        	HttpSession session=request.getSession();  
+	        	session.setAttribute("name",name);  
+	        }  
+	        else{  
+	            out.print("Sorry, username or password error!");  
+	            request.getRequestDispatcher("login.html").include(request, response);  
+	        }  
+	        out.close();  
+    	
+    }  
+}  
--- 18-02-2020/Login/src/com/ibm/training/logout.java
+++ 18-02-2020/Login/src/com/ibm/training/logout.java
@@ -1,24 +1,29 @@
 package com.ibm.training;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
+import java.io.IOException;  
+import java.io.PrintWriter;  
+  
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-
-@WebServlet("/logout")
-public class logout extends HttpServlet {
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        PrintWriter out = response.getWriter();  
-		HttpSession session = request.getSession(false); 
-		session.invalidate();
-		out.println("Logged out");
-	}
-
-}
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+import javax.servlet.http.HttpSession; 
+@WebServlet("/logoutpage")
+public class logout extends HttpServlet {  
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)  
+                                throws ServletException, IOException {  
+            response.setContentType("text/html");  
+            PrintWriter out=response.getWriter();  
+              
+            request.getRequestDispatcher("link.html").include(request, response);  
+              
+            HttpSession session=request.getSession();  
+//            session.removeAttribute("name");
+            session.invalidate();  
+              
+            out.print("You are successfully logged out!");  
+              
+            out.close();  
+    }  
+}  
